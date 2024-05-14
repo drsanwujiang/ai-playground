264d8462dfd7b57f4c7ca8254168fa5b1e52a7a3
Drsanwujiang <drsanwujiang@gmail.com>
2024-05-14 10:16:42 +0800

    Update model: GPT-4o

--- app/chat/gpt/page.tsx
+++ app/chat/gpt/page.tsx
@@ -11,6 +11,7 @@ import { requestGPT } from '@/app/services';
 
 export default function Page() {
     const models: SelectItem[] = [
+        { content: "GPT-4o", value: "gpt-4o" },
         { content: "GPT-4 Turbo", value: "gpt-4-turbo" },
         { content: "GPT-4", value: "gpt-4" },
         { content: "GPT-3.5 Turbo", value: "gpt-3.5-turbo" },
